b300602531a40dc18f9fd7290a9fc047036617f9
sbarreiros <silvino.barreiros@gmail.com>
2018-05-19 18:25:53 -0400

    yuuuup

--- src/main/scala/org/pugsnorts/nn/ProblemFifteen.scala
+++ src/main/scala/org/pugsnorts/nn/ProblemFifteen.scala
@@ -23,4 +23,21 @@ object ProblemFifteen {
 
     buffer.toList
   }
+
+  def duplicateNV2[A](n: Int, list: List[A]): List[A] = {
+    import scala.collection.mutable.ListBuffer
+    
+    def dupicateHalp(a: A): List[A] = {
+      List.fill(n)(a)
+    }
+
+    val buffer = new ListBuffer[A]()
+    var i = 0
+
+    for (i <- 0 until list.length) {
+      dupicateHalp(list(i)).foreach(j => buffer += j)
+    }
+
+    buffer.toList
+  }
 }
