a3c1ead8f285c751146344a430df9e638941cbe2
sbarreiros <silvino.barreiros@gmail.com>
2018-05-19 16:52:16 -0400

    we good

--- src/main/scala/org/pugsnorts/nn/ProblemOne.scala
+++ src/main/scala/org/pugsnorts/nn/ProblemOne.scala
@@ -4,7 +4,21 @@ package org.pugsnorts.nn
  * Find the last element of a list.
  */
 object ProblemOne {
-  def lastV1(l: List[Int]): Int = {
-    Int.MaxValue
+  
+  /**
+   * return the last element of the list, if empty return Int.MaxValue
+   */
+  def lastV1(list: List[Int]): Int = {
+    var last = Int.MaxValue
+
+    if (list.isEmpty) {
+      return last
+    } else if (list.length == 1) {
+      last = list(0)
+    } else {
+      last = list(list.length - 1)
+    }
+
+    last
   }
 }
--- src/test/scala/org/pugsnorts/nn/ProblemOneSpec.scala
+++ src/test/scala/org/pugsnorts/nn/ProblemOneSpec.scala
@@ -9,7 +9,17 @@ class ProblemOneSpec extends FlatSpec with Matchers {
   }
 
   "last" should "give me back null when the list is empty" in new Fixture { fix =>
-
+    
     fix.last(List.empty[Int]) shouldBe(Int.MaxValue)
+  }
+
+  it should "return the only element in a single element array" in new Fixture { fix =>
+
+    fix.last(List(1)) shouldBe 1
+  }
+
+  it should "return the last element in an array with things" in new Fixture { fix =>
+
+    fix.last(List(1, 2, 3)) shouldBe 3
   }
 }
